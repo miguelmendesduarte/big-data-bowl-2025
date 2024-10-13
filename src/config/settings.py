7a3f0015f77fcb102b1bbcada3a88745b6b6ac0c
miguelmendesduarte <mdu97@hotmail.com>
2024-10-13 13:56:24 +0100

    Refactor settings module

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,22 +7,30 @@ from pydantic_settings import BaseSettings
 
 LOG_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
+BASE_DIR: Path = Path(__file__).parent.parent.parent.absolute()
+
 
 class Settings(BaseSettings):
     """Application settings."""
 
-    # Base data directory
-    DATA_DIR: Path = Field(default=Path("data"))
-
-    # Data subdirectories
-    RAW_DIR: Path = Field(init=False, default=Path("raw"))
-    PROCESSED_DIR: Path = Field(init=False, default=Path("processed"))
+    # Directories
+    DATA_DIR: Path = Path.joinpath(BASE_DIR, "data")
+    RAW_DIR: Path = Path.joinpath(DATA_DIR, "raw")
+    PROCESSED_DIR: Path = Path.joinpath(DATA_DIR, "processed")
 
     # Files
-    GAMES_FILE: Path = Field(default=Path("games.csv"))
-    PLAYERS_FILE: Path = Field(default=Path("players.csv"))
-    PLAYER_PLAYS_FILE: Path = Field(default=Path("player_plays.csv"))
-    PLAYS_FILE: Path = Field(default=Path("plays.csv"))
+    GAMES_FILE: Path = Field(
+        default=Path("games.csv"), description="Path to games file."
+    )
+    PLAYERS_FILE: Path = Field(
+        default=Path("players.csv"), description="Path to players file."
+    )
+    PLAYER_PLAYS_FILE: Path = Field(
+        default=Path("player_plays.csv"), description="Path to player_play file."
+    )
+    PLAYS_FILE: Path = Field(
+        default=Path("plays.csv"), description="Path to plays file."
+    )
     TRACKING_FILES: str = Field(
         default="tracking_week_{week}.csv", description="Template for tracking files."
     )
@@ -32,11 +40,6 @@ class Settings(BaseSettings):
     LOG_FORMAT: str = Field(
         default="{asctime}|{filename:>15s}:{lineno:03d}|{levelname:^7s} - {message}"
     )
-
-    def __post_init__(self) -> None:
-        """Post initialization to set derived paths."""
-        self.RAW_DIR = self.DATA_DIR / "raw"
-        self.PROCESSED_DIR = self.DATA_DIR / "processed"
 
     @field_validator("LOG_LEVEL")
     def validate_log_level(cls, log_level: str) -> str:
@@ -50,9 +53,10 @@ class Settings(BaseSettings):
         """
         if log_level not in LOG_LEVELS:
             raise ValueError(f"Invalid log level: {log_level}")
+
         return log_level
 
-    def get_tracking_file_path(self, week: int) -> Path:
+    def tracking_file_path(self, week: int) -> Path:
         """Get tracking file path for given week.
 
         Args:
@@ -64,9 +68,25 @@ class Settings(BaseSettings):
         Returns:
             Path: Path to tracking file.
         """
-        if week < 1 or week > 9:
+        if not 1 <= week <= 9:
             raise ValueError("Week must be between 1 and 9.")
+
         return Path(self.TRACKING_FILES.format(week=week))
+
+    def get_file_path(self, file: Path, processed: bool = False) -> Path:
+        """Get path to file.
+
+        Args:
+            file (Path): Path to file.
+            processed (bool, optional): Whether to use processed directory.
+                Defaults to False (use raw directory).
+
+        Returns:
+            Path: Path to file.
+        """
+        directory = self.PROCESSED_DIR if processed else self.RAW_DIR
+
+        return Path.joinpath(directory, file)
 
 
 settings = Settings()
