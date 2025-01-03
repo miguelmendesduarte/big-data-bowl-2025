7acffcd3e41ecce9843267c758a6a4e6a3f55a8e
miguelmendesduarte <mdu97@hotmail.com>
2025-01-03 12:49:45 +0000

    Add function to compute weighted disguise score

--- src/metric/metric.py
+++ src/metric/metric.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import numpy as np
 import pandas as pd
 
 from ..config.settings import get_settings
@@ -166,6 +167,66 @@ def transform_columns_play_disguise(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def compute_weighted_disguise_score(df: pd.DataFrame, k: float = 0.4) -> pd.DataFrame:
+    """Compute the weighted disguise score.
+
+    The weighted disguise score is a measure of how well a player disguises
+    their intention (blitz vs coverage) in a play.
+    Frames that are closer to the snap have a higher weight.
+    The weighted disguise score is computed as follows:
+    - The frame disguise is weighted by a factor of exp(-k * time_to_snap)
+    - The weighted frame disguise is summed for all frames in the play
+    - The weighted frame disguise is divided by total weight for all frames in the play
+
+    Args:
+        df (pd.DataFrame): Dataframe with the following columns:
+            - 'gameId': game identifier.
+            - 'playId': play identifier.
+            - 'nflId': player identifier.
+            - 'frameId': frame identifier.
+            - 'frame_disguise': frame disguise metric.
+            - 'num_frames': number of frames in play.
+        k (float, optional): Decay factor. Defaults to 0.4.
+
+    Returns:
+        pd.DataFrame: Dataframe with the weighted disguise score.
+    """
+    df["time"] = df["time"].apply(lambda x: x if "." in x else x + ".000")
+    df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%d %H:%M:%S.%f")
+    df["snap_time"] = df.groupby(["gameId", "playId", "nflId"])["time"].transform("max")
+    df["time_to_snap"] = (df["snap_time"] - df["time"]).dt.total_seconds()
+
+    df["weight"] = np.exp(-k * df["time_to_snap"])
+
+    df["weighted_frame_disguise"] = df["frame_disguise"] * df["weight"]
+    df["total_disguise"] = df.groupby(["gameId", "playId", "nflId"])[
+        "weighted_frame_disguise"
+    ].transform("sum")
+
+    df["sum_weights"] = df.groupby(["gameId", "playId", "nflId"])["weight"].transform(
+        "sum"
+    )
+    df["disguise_score"] = df["total_disguise"] / df["sum_weights"]
+
+    df = df.drop_duplicates(subset=["gameId", "playId", "nflId"])
+
+    df = df[
+        [
+            "gameId",
+            "playId",
+            "nflId",
+            "jerseyNumber",
+            "club",
+            "position",
+            "displayName",
+            "wasInitialPassRusher",
+            "disguise_score",
+        ]
+    ]
+
+    return df
+
+
 def main() -> None:
     """Compute the disguise metric and save the results."""
     settings = get_settings()
@@ -174,6 +235,13 @@ def main() -> None:
     writer = CSVWriter()
 
     blitz_probability_data = reader.read(settings.BLITZ_PROBABILITY_RESULTS_FILE)
+
+    player_plays = reader.read(
+        settings.get_data_file_path(file=settings.PLAYER_PLAYS_FILE)
+    )
+    player_plays["gameId"] = player_plays["gameId"].astype(int)
+    player_plays["playId"] = player_plays["playId"].astype(int)
+    player_plays["nflId"] = player_plays["nflId"].astype(int)
 
     blitz_probability_data["frame_disguise"] = compute_frame_disguise_metric(
         blitz_probability_data
@@ -196,6 +264,30 @@ def main() -> None:
         blitz_probability_data[RELEVANT_COLUMNS_FRAME_DISGUISE],
     )
 
+    weighted_disguise_score = compute_weighted_disguise_score(blitz_probability_data)
+    weighted_disguise_score = weighted_disguise_score.merge(
+        player_plays[
+            [
+                "gameId",
+                "playId",
+                "nflId",
+                "quarterbackHit",
+                "causedPressure",
+                "tackleForALoss",
+            ]
+        ],
+        how="left",
+        on=["gameId", "playId", "nflId"],
+    )
+    weighted_disguise_score = convert_ints_to_positions(
+        weighted_disguise_score, ["position"]
+    )
+
+    writer.write(
+        settings.WEIGHTED_PLAY_DISGUISE_RESULTS_FILE,
+        weighted_disguise_score,
+    )
+
     blitz_probability_data = compute_play_disguise_metric(blitz_probability_data)
 
     blitz_probability_data = remove_unwanted_columns(
@@ -207,13 +299,6 @@ def main() -> None:
         ],
     )
     blitz_probability_data = transform_columns_play_disguise(blitz_probability_data)
-
-    player_plays = reader.read(
-        settings.get_data_file_path(file=settings.PLAYER_PLAYS_FILE)
-    )
-    player_plays["gameId"] = player_plays["gameId"].astype(int)
-    player_plays["playId"] = player_plays["playId"].astype(int)
-    player_plays["nflId"] = player_plays["nflId"].astype(int)
 
     blitz_probability_data = blitz_probability_data.merge(
         player_plays[
